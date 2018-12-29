2ab3d32dc72e5adf167b51ca59ae6605d9ed7a68
zhangluangyi <zhangluangyi@163.com>
2018-12-29 09:53:21 +0800

    拦截器

--- src/main.js
+++ src/main.js
@@ -6,6 +6,45 @@ import router from './router'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import store from './store'
+import axios from 'axios'
+axios.defaults.baseURL = '/api'
+axios.defaults.headers.common['Authorization'] = 'zhangluangyi@163.com'
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+// 使用由库提供的配置的默认值来创建实例
+// 此时超时配置的默认值是 `0`
+var instance = axios.create()
+
+// 覆写库的超时默认值
+// 现在，在超时前，所有请求都会等待 2.5 秒
+instance.defaults.timeout = 2500
+
+// 为已知需要花费很长时间的请求覆写超时设置
+instance.get('/longRequest', {
+  timeout: 5000
+})
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  console.log(config)
+  return config
+}, function (error) {
+  // 对请求错误做些什么
+  console.log(error)
+  return Promise.reject(error)
+})
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+  // 对响应数据做点什么
+  console.log(response)
+  return response
+}, function (error) {
+  // 对响应错误做点什么
+  console.log(error)
+  return Promise.reject(error)
+})
+Vue.prototype.$axios = axios
 
 Vue.use(Element)
 Vue.use(store)
--- src/pages/echarts.vue
+++ src/pages/echarts.vue
@@ -1,11 +1,35 @@
 <template>
-  <div class="echarts-list">echarts</div>
+  <div class="echarts-list">
+    echarts
+    <el-input type="password" v-model="pwd" placeholder="请输入密码"></el-input>
+    <el-button @click="apiClick">api-axios</el-button>
+  </div>
 </template>
 
 <script>
 export default {
   data () {
-    return {}
+    return {
+      pwd: ''
+    }
+  },
+  methods: {
+    apiClick () {
+      let params = {
+        LoginForm: {
+          email: '15900000002',
+          rememberMe: '1',
+          password: this.pwd
+        }
+      }
+      this.$axios.post('user/admin/loginn', params)
+        .then(res => {
+          console.log(res.data)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
   }
 }
 </script>
